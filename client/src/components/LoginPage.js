97a1553d18e8fb035c83427ee1030183dc640637
seunggs <seunggs@gmail.com>
2019-01-25 18:13:19 -0800

    fix the google login link

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { PRISMA_HTTP_URL } from '../utils/constants'
 
 const LoginPage = () => {
   return (
     <div>
       <div>I'm LoginPage</div>
-      <div><a href="http://localhost:4000/auth/google">Login to Google</a></div>
+      <div><a href={`${PRISMA_HTTP_URL}/auth/google`}>Login to Google</a></div>
     </div>
   )
 }
