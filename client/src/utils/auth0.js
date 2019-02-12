7ae38441eb832e5c4362ac232c9092b828edc0ae
Seungchan Lee <seungchanlee@SeungchansMBP2.lan>
2019-02-12 09:33:30 -0800

    fix the boilerplate for deployment

--- client/src/utils/auth0.js
+++ client/src/utils/auth0.js
@@ -3,7 +3,7 @@ import * as auth0 from 'auth0-js'
 const webAuth = new auth0.WebAuth({
   domain: 'sidetrek.auth0.com',
   clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-  redirectUri: `${process.env.REACT_APP_CLIENT_URI}/callback`,
+  redirectUri: `${window.location.protocol}//${window.location.hostname}${process.env.NODE_ENV !== 'production' ? ':' + window.location.port : ''}/callback`,
   responseType: 'id_token token',
   audience: 'https://sidetrek.com', // API identifier
   scope: 'openid profile',
